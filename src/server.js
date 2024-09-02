d837a47e2204beb702ab5f9543ee8b63794b47ec
PatcharaPN <patchara.pn31@gmail.com>
2024-09-02 21:40:29 +0700

    First commit

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
-
+//Listen Port
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
